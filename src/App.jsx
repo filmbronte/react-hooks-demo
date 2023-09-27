b02095b51457f9eb0e34f92fcc576c9785fef606
filmbronte <122818201+filmbronte@users.noreply.github.com>
2023-09-27 15:51:27 +0300

    added bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,16 @@
-import { useState } from 'react'
-import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Navigation from './components/Navbar/Navbar';
+import ExampleCard from './components/ExampleCard/ExampleCard';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
+      <Navigation/>
+      <ExampleCard/>
     </>
+
   )
 }
 
